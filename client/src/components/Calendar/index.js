fa854f0ec83aa639077ac89d4eee207588f364d0
ValVarius <60993964+ValVarius@users.noreply.github.com>
2022-11-18 17:41:28 -0700

    fixed some css

--- client/src/components/Calendar/index.js
+++ client/src/components/Calendar/index.js
@@ -1,65 +1,83 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./style.css";
 import $ from "jquery";
 
+
 export default function Calendar() {
-  let daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  const [days, setdays] = useState();
 
-  let d = new Date();
+  useEffect(() => {
+    let daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
-  setDate(d);
-  setYears(5); // set the next five years in dropdown
+    let d = new Date();
 
-  $("#select-month").change(function () {
-    let monthIndex = $("#select-month").val();
-    setDays(monthIndex);
-  });
+    setDate(d);
+    setYears(5); // set the next five years in dropdown
 
-  function setDate(date) {
-    setDays(date.getMonth());
-    $("#select-day").val(date.getDate());
-    $("#select-month").val(date.getMonth());
-    $("#select-year").val(date.getFullYear());
-  }
-  // make sure the number of days correspond with the selected month
-  function setDays(monthIndex) {
-    let optionCount = $("#select-day option").length,
-      daysCount = daysInMonth[monthIndex];
+    $("#select-month").change(function () {
+      let monthIndex = $("#select-month").val();
+      setDays(monthIndex);
+    });
 
-    if (optionCount < daysCount) {
-      for (let i = optionCount; i < daysCount; i++) {
-        $("#select-day").append(
+    function setDate(date) {
+      setDays(date.getMonth());
+      $("#select-day").val(date.getDate());
+      $("#select-month").val(date.getMonth());
+      $("#select-year").val(date.getFullYear());
+    }
+
+    // make sure the number of days correspond with the selected month
+    function setDays(monthIndex) {
+      var optionCount = $("#select-day option").length,
+        daysCount = daysInMonth[monthIndex];
+
+      if (optionCount < daysCount) {
+        for (var i = optionCount; i < daysCount; i++) {
+          $("#select-day").append(
+            $("<option></option>")
+              .attr("value", i + 1)
+              .text(i + 1)
+          );
+        }
+      } else {
+        for (var i = daysCount; i < optionCount; i++) {
+          var optionItem = "#select-day option[value=" + (i + 1) + "]";
+          $(optionItem).remove();
+        }
+      }
+    }
+    // function setDays(monthIndex) {
+    //   let options = [];
+    //   let optionCount = $("#select-day option").length;
+    //   let daysCount = daysInMonth[monthIndex];
+
+    //   for (let i = optionCount; i < daysCount; i++) {
+    //     options.push({ value: i + 1, label: i + 1 });
+    //   }
+
+    //   setdays(options);
+    // }
+
+    function setYears(val) {
+      let year = d.getFullYear();
+      for (let i = 0; i < val; i++) {
+        $("#select-year").append(
           $("<option></option>")
-            .attr("value", i + 1)
-            .text(i + 1)
+            .attr("value", year - i)
+            .text(year - i)
         );
       }
-    } else {
-      for (let i = daysCount; i < optionCount; i++) {
-        let optionItem = "#select-day option[value=" + (i + 1) + "]";
-        $(optionItem).remove();
-      }
     }
-  }
 
-  function setYears(val) {
+    let month = d.getMonth() + 1;
+    let day = d.getDate();
     let year = d.getFullYear();
-    for (let i = 0; i < val; i++) {
-      $("#select-year").append(
-        $("<option></option>")
-          .attr("value", year - i)
-          .text(year - i)
-      );
-    }
-  }
-
-  let month = d.getMonth() + 1;
-  let day = d.getDate();
-  let year = d.getFullYear();
-  let today = month + "|" + day + "|" + year;
+    let today = month + "|" + day + "|" + year;
+    console.log(today);
+  }, []);
 
   return (
-    <div className="select-date">
+    <div className="select-date float-right">
       <select id="select-day"></select>
       <select id="select-month">
         <option value="0">January</option>
@@ -75,7 +93,6 @@ export default function Calendar() {
         <option value="10">November</option>
         <option value="11">December</option>
       </select>
-
       <select id="select-year"></select>
       <button className="btn btn-outline-danger" id="daysearch">
         Search
