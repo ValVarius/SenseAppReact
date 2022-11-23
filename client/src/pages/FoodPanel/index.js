1f885a37a6aad8df410ad191b28f0d23fff5b77b
ValVarius <60993964+ValVarius@users.noreply.github.com>
2022-11-22 17:19:37 -0700

    created route and code to display data from menu

--- client/src/pages/FoodPanel/index.js
+++ client/src/pages/FoodPanel/index.js
@@ -1,41 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./style.css";
 import API from "../../utils/API";
-const { menu } = require('./menu.js')
+const { menu } = require("./menu.js");
 
 export default function FoodPanel() {
+  const [menu, setMenu] = useState();
 
-    // attempt to store the menu
-    // then retrieve the menu and display it
-    // console.log(menu);
-    
-    // API.storeUserMenu(menu).then((newMeal) => {
-    //   console.log(newMeal.data);
-    // });
-    // API.getMenu().then((result) => {
-    //   console.log(result);
-    // });
+  useEffect(() => {
+    API.getMenu().then((result) => {
+      let info = [];
+      result.data.forEach((element) => {
+        info.push({
+          name: element.name,
+          category: element.category,
+          color: element.color,
+          id:element._id
+        });
+      });
+      setMenu(info);
+    });
+  }, []);
+
+  // API.storeUserMenu(menu).then((newMeal) => {
+  //   console.log(newMeal.data);
+  // });
 
   return (
     <div className="container" id="panelContainer">
-      <h1>Veggies</h1>
-      <button>ACORN SQUASH</button>
-      <button>BOK CHOY</button>
-      <button>CAULIFLOWER</button>
-      <button>ACORN SQUASH</button>
-      <button>BOK CHOY</button>
-      <button>CAULIFLOWER</button>
-      <button>ACORN SQUASH</button>
-      <button>BOK CHOY</button>
-      <button>CAULIFLOWER</button>
-      <button>ACORN SQUASH</button>
-      <button>BOK CHOY</button>
-      <button>CAULIFLOWER</button>
+      
 
-      <h1>Title</h1>
-      <button>ACORN SQUASH</button>
-      <button>BOK CHOY</button>
-      <button>CAULIFLOWER</button>
+      {menu ? menu.map(item => <button key={item.id}>{item.name}</button>) : ""}
+
     </div>
   );
 }
