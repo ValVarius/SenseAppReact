7b0d560f2cadf654da9ebb1021128c73ca1f0cb2
ValVarius <60993964+ValVarius@users.noreply.github.com>
2022-11-23 11:14:18 -0700

    added display menu by category

--- client/src/pages/FoodPanel/index.js
+++ client/src/pages/FoodPanel/index.js
@@ -1,36 +1,95 @@
 import React, { useState, useEffect } from "react";
 import "./style.css";
 import API from "../../utils/API";
-const { menu } = require("./menu.js");
+// const { menuStart } = require("./menuStart.js");
 
 export default function FoodPanel() {
   const [menu, setMenu] = useState();
 
   useEffect(() => {
     API.getMenu().then((result) => {
-      let info = [];
-      result.data.forEach((element) => {
-        info.push({
-          name: element.name,
-          category: element.category,
-          color: element.color,
-          id:element._id
-        });
-      });
-      setMenu(info);
+      setMenu(result.data);
     });
   }, []);
 
-  // API.storeUserMenu(menu).then((newMeal) => {
+  // API.storeUserMenu(menuStart).then((newMeal) => {
   //   console.log(newMeal.data);
   // });
 
   return (
     <div className="container" id="panelContainer">
-      
-
-      {menu ? menu.map(item => <button key={item.id}>{item.name}</button>) : ""}
-
+      <h1>Vegetables</h1>
+      {menu
+        ? menu.map((item) => {
+            if (item.category === "Vegetable") {
+              return <button key={item._id}>{item.name}</button>;
+            }
+            else return ""
+          })
+        : ""}
+        <h1>Fruit</h1>
+      {menu
+        ? menu.map((item) => {
+            if (item.category === "Fruit") {
+              return <button key={item._id}>{item.name}</button>;
+            }
+            else return ""
+          })
+        : ""}
+      <h1>Meats</h1>
+      {menu
+        ? menu.map((item) => {
+            if (item.category === "Meat") {
+              return <button key={item._id}>{item.name}</button>;
+            }
+            else return ""
+          })
+        : ""}
+        <h1>Dairy/Eggs</h1>
+      {menu
+        ? menu.map((item) => {
+            if (item.category === "Dairy/Eggs") {
+              return <button key={item._id}>{item.name}</button>;
+            }
+            else return ""
+          })
+        : ""}
+        <h1>Grain/Starch</h1>
+      {menu
+        ? menu.map((item) => {
+            if (item.category === "Grain/Starch") {
+              return <button key={item._id}>{item.name}</button>;
+            }
+            else return ""
+          })
+        : ""}
+         <h1>Herb/Spice</h1>
+      {menu
+        ? menu.map((item) => {
+            if (item.category === "Grain/Starch") {
+              return <button key={item._id}>{item.name}</button>;
+            }
+            else return ""
+          })
+        : ""}
+      <h1>Seafood</h1>
+      {menu
+        ? menu.map((item) => {
+            if (item.category === "Seafood") {
+              return <button key={item._id}>{item.name}</button>;
+            }
+            else return ""
+          })
+        : ""}
+      <h1>Nuts/OilsMiscFood</h1>
+      {menu
+        ? menu.map((item) => {
+            if (item.category === "Nuts/OilsMiscFood") {
+              return <button key={item._id}>{item.name}</button>;
+            }
+            else return ""
+          })
+        : ""}
     </div>
   );
 }
