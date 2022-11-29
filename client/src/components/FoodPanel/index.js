54aa16348789f4dbb2b0eb8421f08f7f9487c6b2
ValVarius <60993964+ValVarius@users.noreply.github.com>
2022-11-28 18:03:59 -0700

    food button functionality

--- client/src/components/FoodPanel/index.js
+++ client/src/components/FoodPanel/index.js
@@ -9,6 +9,7 @@ export default function FoodPanel(props) {
   // })
   const [menu, setMenu] = useState();
   const [searchedItem, setSearchedItem] = useState();
+  const [searchedCategory, setSearchedCategory] = useState();
 
   // setSearchedItem("");
 
@@ -16,7 +17,6 @@ export default function FoodPanel(props) {
     API.getMenu().then((result) => {
       setMenu(result.data);
     });
-    console.log(props);
   }, []);
 
   // API.storeUserMenu(menuStart).then((newMeal) => {
@@ -32,159 +32,15 @@ export default function FoodPanel(props) {
       food: props.food.concat(event.target.value),
     }));
     setSearchedItem("");
+    setSearchedCategory("");
+  };
+
+  let handleSearchedCategory = (event) => {
+    setSearchedCategory(event.target.value);
+    console.log(event.target.value);
   };
 
   return (
-    // <div className="container" id="panelContainer">
-    //   <div className="row">
-    //     <div className="col">
-    //       <h2 className="menutitle"> Vegetables/Legumes</h2>
-    //       {menu
-    //         ? menu.map((item) => {
-    //             if (item.category === "Vegetable") {
-    //               return (
-    //                 <button
-    //                   type="button"
-    //                   className="btn btn-success menubutton"
-    //                   key={item._id}
-    //                 >
-    //                   {item.name}
-    //                 </button>
-    //               );
-    //             } else return "";
-    //           })
-    //         : ""}
-    //     </div>
-    //     <div className="col">
-    //       <h2 className="menutitle"> Fruit</h2>
-    //       {menu
-    //         ? menu.map((item) => {
-    //             if (item.category === "Fruit") {
-    //               return (
-    //                 <button
-    //                   type="button"
-    //                   className="btn btn-success menubutton"
-    //                   key={item._id}
-    //                 >
-    //                   {item.name}
-    //                 </button>
-    //               );
-    //             } else return "";
-    //           })
-    //         : ""}
-    //     </div>
-    //     <div className="col">
-    //       <h2 className="menutitle"> Meats</h2>
-    //       {menu
-    //         ? menu.map((item) => {
-    //             if (item.category === "Meat") {
-    //               return (
-    //                 <button
-    //                   type="button"
-    //                   className="btn btn-success menubutton"
-    //                   key={item._id}
-    //                 >
-    //                   {item.name}
-    //                 </button>
-    //               );
-    //             } else return "";
-    //           })
-    //         : ""}
-
-    //       <h2 className="menutitle"> Dairy/Eggs</h2>
-    //       {menu
-    //         ? menu.map((item) => {
-    //             if (item.category === "Dairy/Eggs") {
-    //               return (
-    //                 <button
-    //                   type="button"
-    //                   className="btn btn-success menubutton"
-    //                   key={item._id}
-    //                 >
-    //                   {item.name}
-    //                 </button>
-    //               );
-    //             } else return "";
-    //           })
-    //         : ""}
-
-    //       <h2 className="menutitle"> Grain/Starch</h2>
-    //       {menu
-    //         ? menu.map((item) => {
-    //             if (item.category === "Grain/Starch") {
-    //               return (
-    //                 <button
-    //                   type="button"
-    //                   className="btn btn-success menubutton"
-    //                   key={item._id}
-    //                 >
-    //                   {item.name}
-    //                 </button>
-    //               );
-    //             } else return "";
-    //           })
-    //         : ""}
-    //     </div>
-
-    //     <div className="col">
-    //       <h2 className="menutitle"> Herb/Spice</h2>
-    //       {menu
-    //         ? menu.map((item) => {
-    //             if (item.category === "Herb/Spice") {
-    //               return (
-    //                 <button
-    //                   type="button"
-    //                   className="btn btn-success menubutton"
-    //                   key={item._id}
-    //                 >
-    //                   {item.name}
-    //                 </button>
-    //               );
-    //             } else return "";
-    //           })
-    //         : ""}
-    //     </div>
-    //     <div className="col">
-    //       <h2 className="menutitle"> Seafood</h2>
-    //       {menu
-    //         ? menu.map((item) => {
-    //             if (item.category === "Seafood") {
-    //               return (
-    //                 <button
-    //                   type="button"
-    //                   className="btn btn-success menubutton"
-    //                   key={item._id}
-    //                 >
-    //                   {item.name}
-    //                 </button>
-    //               );
-    //             } else return "";
-    //           })
-    //         : ""}
-    //     </div>
-
-    //     <div className="col">
-    //       <h2 className="menutitle"> Nuts/OilsMiscFood</h2>
-    //       {menu
-    //         ? menu.map((item) => {
-    //             if (item.category === "Nuts/OilsMiscFood") {
-    //               return (
-    //                 <button
-    //                   type="button"
-    //                   className="btn btn-success menubutton"
-    //                   key={item._id}
-    //                 >
-    //                   {item.name}
-    //                 </button>
-    //               );
-    //             } else return "";
-    //           })
-    //         : ""}
-    //     </div>
-    //   </div>
-    // </div>
-
-    // container
     <>
       <div className="row">
         <input
@@ -201,8 +57,8 @@ export default function FoodPanel(props) {
           className="form-select"
           aria-label="Default select example"
           id="category-select"
-          // name=""
-          // onChange={handleChange}
+          name=""
+          onChange={handleSearchedCategory}
         >
           <option value="Vegetable">Vegetables</option>
           <option value="Fruit">Fruit</option>
@@ -214,10 +70,29 @@ export default function FoodPanel(props) {
           <option value="Nuts/OilsMiscFood">Nuts/OilsMiscFood</option>
         </select>
       </div>
+      <div>
+        {searchedCategory
+          ? menu.map((item) => {
+              if (item.category === searchedCategory) {
+                return (
+                  <button
+                    type="button"
+                    className="btn btn-success menubutton"
+                    key={item._id}
+                    name="food"
+                    value={item.name}
+                    onClick={addItem}
+                  >
+                    {item.name}
+                  </button>
+                );
+              } else return "";
+            })
+          : ""}
+      </div>
+
       {searchedItem
         ? menu.map((result) => {
-            //     return (result.name).substring(0,searchedItem.length).toUpperCase() === searchedItem.toUpperCase()
-
             if (
               result.name.substring(0, searchedItem.length).toUpperCase() ===
               searchedItem.toUpperCase()
