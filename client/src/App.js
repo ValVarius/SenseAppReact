e8af77d8801d154b04a69165a92f5d86da7a9ca7
ValVarius <60993964+ValVarius@users.noreply.github.com>
2022-12-05 19:08:21 -0700

    fixed background img and css

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,8 @@ import PdfPage from "./pages/PdfPage";
 import FoodPanel from "./components/FoodPanel";
 import Calendar from "./components/Calendar";
 import Navbar from "./components/Navbar";
-import background from "./public/newdots.jpg";
 import React, { useState, useEffect } from "react";
+
 
 function App() {
   // This should be passed to the form and calendar component
@@ -24,18 +24,11 @@ function App() {
       date: d.getMonth() + 1 + "|" + d.getDate() + "|" + d.getFullYear(),
     });
   }, []);
-
   return (
-    <div
-      style={{
-        backgroundImage: `url(${background})`,
-        backgroundPosition: "center",
-        backgroundSize: "cover",
-        backgroundRepeat: "no-repeat",
-        width: "100vw",
-        height: "100vh",
-      }}
+    <div id="app" 
+    
     >
+      
       <Navbar date={date} setDate={setDate} />
       {date.date ? <Home date={date} setDate={setDate} /> : null}
       {/* {date.date ? <Calendar date={date} setDate={setDate}/> : ""} */}
