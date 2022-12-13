5bb8f129ea311c73e6a1841777996363a1097394
ValVarius <60993964+ValVarius@users.noreply.github.com>
2022-12-13 12:48:17 -0700

    redesigned user model

--- models/user.js
+++ models/user.js
@@ -2,47 +2,60 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const bcrypt = require("bcrypt");
 SALT_WORK_FACTOR = 10;
-const Meal = require('./meal');
+const Meal = require("./meal");
+
+let validateEmail = function(email) {
+  var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return re.test(email)
+};
 
 const UserSchema = new Schema({
   username: { type: String, required: true },
+  first: { type: String, required: true },
+  last: { type: String, required: true },
+  email: {
+    type: String,
+    trim: true,
+    lowercase: true,
+    unique: true,
+    required: "Email address is required",
+    validate: [validateEmail, "Please fill a valid email address"],
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      "Please fill a valid email address",
+    ],
+  },
   password: { type: String, required: true },
-  meals: [{ type: Schema.Types.ObjectId, ref: 'Meal' }]
+  meals: [{ type: Schema.Types.ObjectId, ref: "Meal" }],
 });
 
-
-UserSchema.pre("save", function(next) {
+UserSchema.pre("save", function (next) {
   var user = this;
 
-// only hash the password if it has been modified (or is new)
-if (!user.isModified('password')) return next();
+  // only hash the password if it has been modified (or is new)
+  if (!user.isModified("password")) return next();
 
-// generate a salt
-bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
-  if (err) return next(err);
+  // generate a salt
+  bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
+    if (err) return next(err);
 
-  // hash the password using our new salt
-  bcrypt.hash(user.password, salt, function(err, hash) {
+    // hash the password using our new salt
+    bcrypt.hash(user.password, salt, function (err, hash) {
       if (err) return next(err);
 
       // override the cleartext password with the hashed one
       user.password = hash;
       next();
-});
-});
-
-
+    });
+  });
 });
 
-UserSchema.methods.comparePassword = function(candidatePassword, cb) {
-  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
-      if (err) return cb(err);
-      cb(null, isMatch);
+UserSchema.methods.comparePassword = function (candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
   });
 };
-
-
-
 
 const User = mongoose.model("User", UserSchema);
 
