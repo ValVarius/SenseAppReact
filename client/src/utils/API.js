63ecf387c5e5825ff1247b3b134584453ef61340
ValVarius <60993964+ValVarius@users.noreply.github.com>
2022-12-16 16:48:24 -0700

    fixing baseurl for deployment

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = process.env.PUBLIC_URL || "http://localhost:3001";
+// const BASE_URL = process.env.PUBLIC_URL || "http://localhost:3001";
+const BASE_URL = "https://sensapp.herokuapp.com";
 const API = {
   mealRegistration: function (data) {
     return axios.post(`${BASE_URL}/api/meals`, data);
