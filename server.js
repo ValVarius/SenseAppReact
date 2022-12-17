703aff9eaec895cfe4d6d3003b626d40a908aa7d
ValVarius <60993964+ValVarius@users.noreply.github.com>
2022-12-16 17:39:34 -0700

    wip

--- server.js
+++ server.js
@@ -34,9 +34,9 @@ if (process.env.NODE_ENV === "production") {
   // app.use(express.static("client/build"));
 
   // Anything that doesn't match the above, send back index.html
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-  });
+  // app.get("*", (req, res) => {
+  //   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  // });
 } else {
   app.get("/", (req, res) => {
     res.send("API LISTENING");
