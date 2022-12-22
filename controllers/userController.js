e862a1870fb252e71074a4847225910ca42622be
ValVarius <60993964+ValVarius@users.noreply.github.com>
2022-12-21 19:10:08 -0700

    adding user to session

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,71 +3,84 @@ const bcrypt = require("bcrypt");
 
 // Defining methods for the UserController
 module.exports = {
-  findAll: function(req, res) {
-    db.User
-      .find(req.query)
+  findAll: function (req, res) {
+    db.User.find(req.query)
       .populate("meals")
       .sort({ date: -1 })
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
   },
-  findById: function(req, res) {
-    db.User
-      .findById(req.params.id)
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
+  findById: function (req, res) {
+    db.User.findById(req.params.id)
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
   },
-  create: function(req, res) {
-    db.User
-      .create(req.body)
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
+  create: function (req, res) {
+    db.User.create(req.body)
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
   },
-  update: function(req, res) {
-    db.User
-      .findOneAndUpdate({ _id: req.params.id }, req.body)
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
+  update: function (req, res) {
+    db.User.findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
   },
-  remove: function(req, res) {
-    db.User
-      .findById({ _id: req.params.id })
-      .then(dbModel => dbModel.remove())
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
+  remove: function (req, res) {
+    db.User.findById({ _id: req.params.id })
+      .then((dbModel) => dbModel.remove())
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
   },
-  login: function(req, res) {
-    db.User
-      .findOne({username:req.body.username})
+  login: function (req, res) {
+    db.User.findOne({ username: req.body.username })
       .populate("meals")
-    //   .sort({ date: -1 })
-      .then(dbUser => {
-        // console.log(dbUser);
-        // if (req.session.user) {
-        //     res.json(dbUser)
-        // }
-        if (dbUser===null) {
-            // req.session.user = false
-            console.log("WRONG USER");
-            res.send("no user found")
-        }
-        else if (bcrypt.compareSync(req.body.password, dbUser.password)) {
-            // req.session.user = dbUser
-            // console.log("JUST CREATED SESSION ", req);
-            console.log("PASSWORD MATCH");
+      //   .sort({ date: -1 })
+      .then((dbUser) => {
+
+        console.log(req.session);
+        if (req.session.user) {
             res.json(dbUser)
         }
-        
-        else {
-            // req.session.user = false
-            console.log("WRONG password");
-            res.send("incorrect password")
+
+      //   req.session.user = {
+      //     uuid: '12234-2345-2323423'
+      // } //THIS SETS AN OBJECT - 'USER'
+      // req.session.save(err => {
+      //     if(err){
+      //         console.log(err);
+      //     } else {
+      //         res.send(req.session.user) // YOU WILL GET THE UUID IN A JSON FORMAT
+      //     }
+      // });
+
+
+
+        else if (!dbUser) {
+          req.session.user = false
+          console.log("WRONG USER");
+          res.send("no user found");
+        } else if (bcrypt.compareSync(req.body.password, dbUser.password)) {
+          req.session.user = dbUser
+          console.log("JUST CREATED SESSION:", req.session);
+          console.log("PASSWORD MATCH");
+          res.json(dbUser);
+        } else {
+          req.session.user = false
+          console.log("WRONG password");
+          res.send("incorrect password");
         }
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
-        res.status(422).json(err)
-        
-    });
-  }
+        res.status(422).json(err);
+      });
+  },
+  readSession: function (req, res) {
+    console.log("IN THE READ SESSION");
+    res.json(req.session);
+  },
+  logout: function (req, res) {
+    req.session.destroy();
+    res.json("logged out!");
+  },
 };
