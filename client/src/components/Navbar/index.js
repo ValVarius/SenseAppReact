858bbad7a72d97d778432da0cc4a51e07edd965d
ValVarius <60993964+ValVarius@users.noreply.github.com>
2023-01-05 11:26:16 -0700

    remoduling navbar

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -3,9 +3,12 @@ import "./style.css";
 import Calendar from "../Calendar";
 import { useNavigate } from "react-router-dom";
 import API from "../../utils/API";
+import { useLocation } from "react-router-dom";
 
 export default function Navbar(props) {
   const navigate = useNavigate();
+  const location = useLocation();
+  console.log(location.pathname);
 
   const handleLogoutClick = (event) => {
     API.logout().then((res) => {
@@ -17,16 +20,21 @@ export default function Navbar(props) {
     event.preventDefault();
     navigate("/Stats");
   };
+  const goHome = (event) => {
+    event.preventDefault();
+    navigate("/Home");
+  };
   return (
     <nav className="navbar navbar-expand" aria-label="">
       <div className="container-fluid">
-        <a href="/Home" className="navbar-brand">
-          <img
-            src="https://cellsciencesystems.com/images/cell-science-systems-logo.png"
-            alt="cell science systems logo"
-            id="navbarlogo"
-          />
-        </a>
+        {/* <a href="/Home" className="navbar-brand"> */}
+        <img
+          src="https://cellsciencesystems.com/images/cell-science-systems-logo.png"
+          alt="cell science systems logo"
+          id="navbarlogo"
+          onClick={goHome}
+        />
+        {/* </a> */}
 
         {props.currentUser ? (
           <>
@@ -38,23 +46,36 @@ export default function Navbar(props) {
             >
               LogOut
             </button>
+            {location.pathname === "/Home" ? (
+              <button
+                type="button"
+                className="btn btn-light navbtn"
+                id="statslink"
+                onClick={toStats}
+              >
+                <img
+                  src={require("../../public/stats.png")}
+                  alt="Stats Logo"
+                  id="statlogo"
+                />
+                Statistics
+              </button>
+            ) : (
+              ""
+            )}
 
-            <button
-              type="button"
-              className="btn btn-light navbtn"
-              id="statslink"
-              onClick={toStats}
-            >
-              <img
-                src={require("../../public/stats.png")}
-                alt="Stats Logo"
-                id="statlogo"
-              />
-              Statistics
-            </button>
-            {props.date.date ? (
+            {props.date.date && location.pathname === "/Home" ? (
               <Calendar date={props.date} setDate={props.setDate} />
-            ) : null}
+            ) : (
+              <button
+                className="btn navbtn"
+                id="backbutton"
+                type="button"
+                onClick={goHome}
+              >
+                Back
+              </button>
+            )}
           </>
         ) : null}
       </div>
